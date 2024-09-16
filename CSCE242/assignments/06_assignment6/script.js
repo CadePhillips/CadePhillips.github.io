423b680bf2a0c1526d1a33020435d6d41aed54c5
Cade Phillips <cadeap@email.sc.edu>
2024-09-15 21:24:44 -0400

    Completed assignment 6

--- CSCE242/assignments/06_assignment6/script.js
+++ CSCE242/assignments/06_assignment6/script.js
@@ -1,6 +1,42 @@
-for (let count = 0; count < 1; count++){
-    document.getElementById("count").onclick = () => {
-            document.getElementById("count").innerHTML = count+1;
+document.addEventListener("DOMContentLoaded", () => {
+
+    const slider = document.getElementById("slider");
+const moveSquare = document.getElementById("section3-pic");
+const container = document.getElementById("content-slider");
+
+if (slider && moveSquare && container) {
+    slider.value = 0;
+    function updateMoveSquare() {
+        const sliderValue = slider.value;
+        const maxMove = container.offsetWidth - moveSquare.offsetWidth;
+        const newLeft = (sliderValue / 100) * maxMove;
+        moveSquare.style.left = `${newLeft}px`;
+    }
+    /*
+    updateMoveSquare();
+    slider.addEventListener('input', updateMoveSquare);
+    */
+
+function setInitialValue() {
+    slider.value = 0;
+    updateMoveSquare();
+}
+
+setInitialValue();
+slider.addEventListener("input", updateMoveSquare);
+window.addEventListener("resize", () => {
+    setInitialValue();
+});
+}
+});
+
+for (let i = 0; i <= 1; i++){
+    document.getElementById("main-section1").onclick = () => {
+        document.getElementById("count").innerHTML = i++;
     }
 }
 
+const refresh = document.getElementById("section2-pic");
+refresh.onclick = () => {
+    window.location.reload();
+}
